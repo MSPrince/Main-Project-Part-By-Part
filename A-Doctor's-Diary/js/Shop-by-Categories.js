02a338bb4e73c4c540b03217edc6568363806d82
M S Prince <135182786+MSPrince@users.noreply.github.com>
2024-03-02 19:54:43 +0530

    update code

---
+++ A-Doctor's-Diary/js/Shop-by-Categories.js
@@ -0,0 +1,50 @@
+const products = [
+    { name: 'Top Product', link: 'fdvdfvdv', imageSrc: './asset/Shop-by-Categories/accessories & wearables.webp' },
+    { name: 'Elderly care', link: 'sdgdgdsgsd', imageSrc: './asset/Shop-by-Categories/Elderly-care.webp' },
+    { name: 'Personal care', link: 'vdsvsd', imageSrc: './asset/Shop-by-Categories/Personal-care.webp' },
+    { name: 'Healthcare devices', link: 'sfsfd', imageSrc: './asset/Shop-by-Categories/Healthcare-devices.webp' },
+    { name: 'Health food and drinks', link: 'dvfsdvsd', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Skin care', link: 'dfsdfgs', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Mother and baby care', link: 'fsdes', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Accessories & wearables', link: 'advgfrger', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Fitness supplements', link: 'fvgrt5g', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Ayurvedic care', link: 'egert', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Sexual wellness', link: 'rw456t45', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Mega clearance sale', link: 'wet656t4t', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Diabetic care', link: 'fdsczczxcsa', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Health condition', link: 'wwerwerwer', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Winter store', link: 'eerwrew', imageSrc: './asset/Shop-by-Categories/top-product.webp' },
+    { name: 'Trending products', link: '', imageSrc: './asset/Shop-by-Categories/top-product.webp' }
+
+    // Add more products as needed
+  ];
+
+  // Reference to the product container
+  const productContainer = document.getElementById('product-container');
+
+  // Dynamically generate product containers and content
+  products.forEach(product => {
+    const producttContainer = document.createElement('div');
+    producttContainer.classList.add('productt-container');
+
+    const aacDiv = document.createElement('div');
+    aacDiv.classList.add('aac');
+
+    const img = document.createElement('img');
+    img.src = product.imageSrc;
+    img.alt = '';
+
+    const anchor = document.createElement('a');
+    anchor.href = product.link;
+    anchor.appendChild(img);
+
+    aacDiv.appendChild(anchor);
+
+    const h5 = document.createElement('h5');
+    h5.textContent = product.name;
+
+    producttContainer.appendChild(aacDiv);
+    producttContainer.appendChild(h5);
+
+    productContainer.appendChild(producttContainer);
+  });
---
+++ Abhi-Pathology-Center/js/script.js
@@ -0,0 +1,12 @@
+// // for hide code 
+// // for hide code
+// document.addEventListener("contextmenu", (e) => {
+//   e.preventDefault();
+//  }, false);
+
+//  document.addEventListener("keydown", (e) => {
+//   if (e.ctrlKey || e.keyCode==123) {
+//    e.stopPropagation();
+//    e.preventDefault();
+//   }
+//  });
