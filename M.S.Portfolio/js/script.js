5448760f96846e24fffb6a1b92f6ee0131f3b55d
M S Prince <135182786+MSPrince@users.noreply.github.com>
2024-03-07 00:03:03 +0530

    update

--- M.S.Portfolio/js/script.js
+++ M.S.Portfolio/js/script.js
@@ -85,3 +85,36 @@ mouse.y = e.clientY;
 create();
 canvas.addEventListener('mousemove', MouseMove, false);
 particles();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for hide code 
+// for hide code
+
+
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+   }, false);
+  
+   document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey || e.keyCode==123) {
+     e.stopPropagation();
+     e.preventDefault();
+    }
+   });
